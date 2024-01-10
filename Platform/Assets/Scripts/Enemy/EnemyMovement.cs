ffc74efc0a93f505617e516e87f1273004df87a6
Luis <luisgmgomes@gmail.com>
2024-01-10 13:59:15 -0300

    feat: enemyMove

---
+++ Platform/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float enemySpeed = 2f;
+    Rigidbody2D enemyRb;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        OnMove();
+    }
+
+    void OnMove()
+    {
+        enemyRb.velocity = new Vector2(enemySpeed, 0f);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            enemySpeed = -enemySpeed;
+            FlipEnemyFacing();
+        }
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(0.5f), 0.5f);
+    }
+}
--- Platform/Assets/Scripts/Player/PlayerMovement.cs
+++ Platform/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,19 +7,16 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 5f;
-    [SerializeField] float jumpSpeed = 8.3f;
-    [SerializeField] float climbSpeed = 5f;
+    [SerializeField] float climbSpeed = 2f;
 
     Rigidbody2D playerRb;
     Vector2 moveInput;
     CapsuleCollider2D playerCaps;
-    float gravityScaleAtStart;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         playerCaps = GetComponent<CapsuleCollider2D>();
-        gravityScaleAtStart = playerRb.gravityScale;
     }
 
     void Update()
@@ -34,26 +31,16 @@ public class PlayerMovement : MonoBehaviour
     }
     void Run()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, playerRb.velocity.y);
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, moveInput.y * playerSpeed);
         playerRb.velocity = playerVelocity;
-    }
-    void OnJump(InputValue value)
-    {
-        if (!playerCaps.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
-        if (value.isPressed)
-        {
-            playerRb.velocity += new Vector2(0f, jumpSpeed);
-        }
     }
     void ClimbLadder()
     {
         if (!playerCaps.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
-            playerRb.gravityScale = gravityScaleAtStart;
             return;
         }
-        Vector2 climbVelocity = new Vector2(playerRb.velocity.x, moveInput.y * climbSpeed);
+        Vector2 climbVelocity = new Vector2(playerRb.velocity.x, Mathf.Abs(transform.position.y * climbSpeed));
         playerRb.velocity = climbVelocity;
-        playerRb.gravityScale = 0;
     }
 }
