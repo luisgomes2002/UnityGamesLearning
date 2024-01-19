fc2097ab39f7b28908ccfe0fc2f9e1dfba790a37
Luis <luisgmgomes@gmail.com>
2024-01-18 23:21:02 -0300

    feat: playerMovement -Climbing

--- Plataform/Assets/Scripts/Player/PlayerMovements.cs
+++ Plataform/Assets/Scripts/Player/PlayerMovements.cs
@@ -7,21 +7,27 @@ public class PlayerMovements : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 5f;
     [SerializeField] float playerJumpForce = 5f;
+    [SerializeField] float playerClimbingSpeed = 5f;
     [SerializeField] int playerScore = 0;
+    float gravityScaleAtStart;
     Rigidbody2D playerRb;
     Vector2 moveInput;
     Animator playerAnimator;
+    CapsuleCollider2D playerCapsuleCollider2D;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        gravityScaleAtStart = playerRb.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        OnClimbing();
     }
 
     void OnMove(InputValue value)
@@ -35,13 +41,13 @@ public class PlayerMovements : MonoBehaviour
         Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, playerRb.velocity.y);
         playerRb.velocity = playerVelocity;
 
-        bool playerHasSpeedPositive = Mathf.Abs(playerRb.velocity.x) > Mathf.Epsilon;
+        bool playerHasSpeedPositive = Mathf.Abs(playerRb.velocity.y) > Mathf.Epsilon;
         playerAnimator.SetBool("isRunning", playerHasSpeedPositive);
     }
 
     void OnJump(InputValue value)
     {
-        if (value.isPressed)
+        if (value.isPressed && playerCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             playerRb.velocity += new Vector2(0f, playerJumpForce);
         }
@@ -64,5 +70,23 @@ public class PlayerMovements : MonoBehaviour
             playerScore++;
             Destroy(col.gameObject);
         }
+    }
+
+    void OnClimbing()
+    {
+        if (!playerCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            playerRb.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        Vector2 playerClimbingVelocity = new Vector2(playerRb.velocity.x, moveInput.y * playerClimbingSpeed);
+        playerRb.velocity = playerClimbingVelocity;
+
+        bool playerHasSpeedPositive = Mathf.Abs(playerRb.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("isClimbing", playerHasSpeedPositive);
+
+        playerRb.gravityScale = 0f;
+
     }
 }
