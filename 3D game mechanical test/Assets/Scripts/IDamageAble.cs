3c73fdda618a2361c9a4df9f50d9a33950fb62a3
Luis <luisgmgomes@gmail.com>
2024-02-14 09:46:37 -0300

    delete

--- 3D game mechanical test/Assets/Scripts/IDamageAble.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageAble
-{
-    public float health { get; set; }
-    public void GetDamage(float damage);
-}
